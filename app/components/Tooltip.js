f5b71a18238e30493c79ee19f8d27bb386306044
Silvi47 <80204242+Silvi47@users.noreply.github.com>
2022-03-16 14:56:29 +0400

    Update Tooltip.js

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,51 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Hover from './Hover'
+import useHover from '../hooks/useHover'
 
 const styles = {
-    container: {
-      position: 'relative',
-      display: 'flex'
-    },
-    tooltip: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      width: '160px',
-      bottom: '100%',
-      left: '50%',
-      marginleft: '-80px',
-      borderRadius: '3px',
-      backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-      padding: '7px',
-      marginBottom: '5px',
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: '14px,'
-    }
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '160px',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-80px',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '7px',
+    marginBottom: '5px',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '14px',
+  }
 }
 
-/*class Tooltip extends React.Component {
-    render() {
-        const { text, children } = this.props
-        const { hovering } = this.state
-    }
-}*/
+export default function Tooltip ({ text, children }) {
+  const [hovering, attrs] = useHover()
 
-function Tooltip ({ text, children }) {
-    return(
-        <Hover>
-            {(hovering) => (
-                <div style={styles.container}>
-                    {hovering === true && <div style={styles.tooltip}>{text}</div>}
-                    {children}
-                </div>
-            )}
-        </Hover>
-    )
+  return (
+    <div style={styles.container} {...attrs}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
 }
 
 Tooltip.propTypes = {
-    text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 }
-
-export default Tooltip;
